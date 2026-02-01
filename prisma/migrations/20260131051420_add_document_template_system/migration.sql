ae1dbb17255cd56604775a07b9c5b7e7668fe0a4
MohamadSolkhanNawawi <mohamadsolkhan558@gmail.com>
2026-02-01 18:03:56 +0700

    fix(db): remove redundant and conflicting migration folder to resolve prisma reset error

--- prisma/migrations/20260131051420_add_document_template_system/migration.sql
+++
@@ -1,5 +0,0 @@
--- AddForeignKey
-ALTER TABLE "template_variable" ADD CONSTRAINT "template_variable_templateId_fkey" FOREIGN KEY ("templateId") REFERENCES "document_template"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "letter_instance" ADD CONSTRAINT "letter_instance_stampId_fkey" FOREIGN KEY ("stampId") REFERENCES "user_stamp"("id") ON DELETE SET NULL ON UPDATE CASCADE;
