65c4d70c58e9ba4e4a675bc146fd70cba86cf28e
setadewaB <setadewab@gmail.com>
2026-02-06 19:57:02 +0700

    feat(config): add or update configuration for frontend URL and environment variables

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,19 @@
 import env from "env-var";
 
 export const config = {
-	NODE_ENV: env
-		.get("NODE_ENV")
-		.default("development")
-		.asEnum(["production", "test", "development"]),
+    NODE_ENV: env
+        .get("NODE_ENV")
+        .default("development")
+        .asEnum(["production", "test", "development"]),
 
-	PORT: env.get("PORT").default(3000).asPortNumber(),
-	API_URL: env
-		.get("API_URL")
-		.default(`https://${env.get("PUBLIC_DOMAIN").asString()}`)
-		.asString(),
-	DATABASE_URL: env.get("DATABASE_URL").required().asString(),
-	LOCK_STORE: env.get("LOCK_STORE").default("memory").asEnum(["memory"]),
+    PORT: env.get("PORT").default(3000).asPortNumber(),
+    API_URL: env
+        .get("API_URL")
+        .default(`https://${env.get("PUBLIC_DOMAIN").asString()}`)
+        .asString(),
+    DATABASE_URL: env.get("DATABASE_URL").required().asString(),
+    LOCK_STORE: env.get("LOCK_STORE").default("memory").asEnum(["memory"]),
+
+    // Frontend URL for QR code verification links
+    FRONTEND_URL: env.get("FE_URL").default("http://localhost:3000").asString(),
 };
