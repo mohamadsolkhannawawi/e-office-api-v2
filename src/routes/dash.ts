1f5fcecaaadb153b5d7f440d23ed29d7714923a8
mohamadsolkhannawawi <mohamadsolkhan558@gmail.com>
2026-03-22 20:07:38 +0700

    docs(dashboard): add comments to dashboard route

--- src/routes/dash.ts
+++ src/routes/dash.ts
@@ -1,11 +1,15 @@
 import { authGuardPlugin } from "@backend/middlewares/auth.ts";
 import { Elysia } from "elysia";
 
+/**
+ * [ROUTE] Dash Route
+ * Endpoint sederhana untuk mengembalikan konteks user terautentikasi.
+ */
 export default new Elysia().use(authGuardPlugin).get(
-	"/",
-	async ({ user }) => {
-		console.log(user);
-		return user;
-	},
-	{},
+  "/",
+  async ({ user }) => {
+    console.log(user);
+    return user;
+  },
+  {},
 );
